e949e9bba1491806d0a7190c5f3ad2fdd46354a3
Chr1st1anJ <77973191+Chr1st1anJ@users.noreply.github.com>
2023-03-15 20:11:00 -0600

    Mejora del titulo

---
+++ admin/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('RUTA', 'http://localhost/xampp1/Proyecto-Catedra-DSS/');
+
+//nombre base de datos
+
+$bd_config = [
+    'db_name' => 'login_curso',
+    'user' => 'root',
+    'pass' => ''
+]
+
+?>
