f6516c2036d90714cf5a1e40c03550dd7959c78c
Fr4nAc323 <124444885+Fr4nAc323@users.noreply.github.com>
2023-03-31 12:08:34 -0600

    ver_movimientos.php

--- usuarios-menu/cliente/registro-cuenta/ver_movimientos.php
+++ usuarios-menu/cliente/registro-cuenta/ver_movimientos.php
@@ -10,11 +10,11 @@ $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="usuarios-menu/cliente/registro-cuenta/ver-diseños.css">
-    <title>Movimientos</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/VerDiseños.css">
+  <title>Movimientos</title>
 </head>
 <body>
     <?php 
